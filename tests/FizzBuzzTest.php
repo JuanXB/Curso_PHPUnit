ca1f4b97b122302deac8d45fd0db4819c1e954c6
Juan C. Bertoia <bertoia114@gmail.com>
2022-05-23 14:14:54 -0300

    Proveedor de datos

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
+  /*
   public function testCuandoLedoyElNumeroTresMeDevuelveFizz(): void
   {
     $fizzBuzz = new FizzBuzz();
@@ -41,5 +42,30 @@ class FizzBuzzTest extends TestCase
     $resultado = $fizzBuzz->diNumero(1);
 
     $this->assertEquals(1, $resultado);
+  }
+  */
+
+  public function casosDeUso(): array
+  {
+    return [
+      [3, 'Fizz'],
+      [5, 'Buzz'],
+      [15, 'FizzBuzz'],
+      [1, 1],
+    ];
+  }
+
+  /**
+   * @dataProvider casosDeUso
+   *  
+   */
+
+  public function testFizzBuzz($numeroATestear, $resultadoEsperado)
+  {
+    $fizzBuzz = new FizzBuzz();
+
+    $resultado = $fizzBuzz->diNumero($numeroATestear);
+
+    $this->assertEquals($resultadoEsperado, $resultado);
   }
 }
